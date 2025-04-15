16735722ac1e60d765ff6a10d6076612c34d75d2
fortune.akinremi <fortuneakinremi@gmail.com>
2025-04-15 14:00:58 -0400

    Changed the app to a timer app -- implemented the basic UI of the time (hour and minute)

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export default function NavBar() {
   return (
-    <div className="flex w-full py-3 px-3 border items-center justify-between gap-20"> 
-      <p>Email.</p>
+    <div className="flex w-full py-3 px-3 border-b border-neutral-600 items-center justify-between gap-20 text-neutral-400"> 
+      <p>Timer.</p>
       <Link href={"https://github.com/limitlez2020"} target="_blank">
         <button className="flex py-1 px-2 border text-sm cursor-pointer">
           Github
--- app/page.js
+++ app/page.js
@@ -5,14 +5,23 @@ import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full h-screen min-h-full bg-neutral-300 justify-center">
+    <div className="flex flex-col w-full h-screen min-h-full bg-neutral-900 justify-center">
       {/* NavBar: */}
       <NavBar/>
 
       {/* Body: */}
-      <div className="flex flex-col w-full h-full bg-neutral-200 justify-center items-center">
-        Enter Email
-        <textarea className="w-1/2 border p-2"/>
+      <div className="flex flex-col w-full h-full bg-neutral-900 justify-center items-center">
+        {/* Time: */}
+        <div className="flex justify-between gap-10 w-4/5 text-neutral-300">
+          {/* Hour: */}
+          <div className="flex justify-center items-center w-1/2 py-10 text-[200px] border border-neutral-400">
+            11
+          </div>
+          {/* Minute: */}
+          <div className="flex justify-center items-center w-1/2 py-10 text-[200px] border border-neutral-400">
+            45
+          </div>
+        </div>
       </div>
     </div>
   );
